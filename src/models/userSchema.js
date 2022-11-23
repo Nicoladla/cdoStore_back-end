f04d0623cc6701c37dc4b92d4665fb70c9b9c9fa
Nicolas Araújo <nicolasaraujo@gmail.com>
2022-11-22 22:55:18 -0300

    feat: modelo base de validação de dados de cadastro

---
+++ src/models/userSchema.js
@@ -0,0 +1,7 @@
+import joi from "joi";
+
+const userSchema = joi.object({
+  name: joi.string().min(3).required(),
+  email: joi.string().email().required(),
+  password: joi.string().min(6).required(),
+});
