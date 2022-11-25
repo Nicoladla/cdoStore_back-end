4a5011a7b92967bb88eac88e8290c2d1e4bfcebf
Nicolas Araújo <nicolasaraujo@gmail.com>
2022-11-25 20:38:46 -0300

    feat: sign-in funcionando.

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -1,5 +1,7 @@
-import { cartsCollection, usersCollection } from "../database/db.js";
 import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
+
+import { cartsCollection, usersCollection } from "../database/db.js";
 
 export async function signUp(req, res) {
   const user = res.locals.user;
@@ -28,7 +30,11 @@ export async function signIn(req, res) {
       return;
     }
 
-    res.send(userExist);
+    const token = jwt.sign({ userId: userExist._id }, process.env.SECRET_JWT, {
+      expiresIn: 60,
+    });
+
+    res.send(token);
   } catch (err) {
     console.log(err);
     res.sendStatus(500);
